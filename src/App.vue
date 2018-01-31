7ece9834c2b14f626cb281490008176cc0295a95
Tony Goldsmith <tony.h.goldsmith@gmail.com>
2018-01-31 08:00:05 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <WordPrompter v-for="word in words" :key='word' :word='word' />
+    <div>
+        <WordList />
+        <WordList />
+        <WordList />
+    </div>
+  </div>
+</template>
+
+<script>
+import words from '../words.js';
+import WordPrompter from './WordPrompter';
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Select Which Applies for The Following Values',
+      words,
+      positives: [],
+      neutrals: [],
+      negatives: [],
+    }
+  },
+  components: {
+      WordPrompter
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/WordPrompter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div>{{word}}</div>
+      <div>
+          <button>Positive</button>
+          <button>Neutral</button>
+          <button>Negative</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordPrompter',
+  props: {
+      word: String,
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
